84c8ab72b17a00c0c0366ce125d8536645509cca
kimywann <kimywan10@gmail.com>
2025-05-19 23:58:38 +0900

    feat: 회원가입 폼 컴포넌트 구조화 및 Props 활용
    - EmailInput, Input 컴포넌트 분리하여 재사용성 향상
    - Props를 통한 부모-자식 컴포넌트 간 데이터 전달 구현
    - 이메일 도메인 선택 기능 구현
    - 폼 유효성 검사 로직 구현

---
+++ example/src/components/EmailInput.jsx
@@ -0,0 +1,31 @@
+export default function EmailInput({
+  errors,
+  idRef,
+  id,
+  domain,
+  onChangeEmail,
+  onChangeDomain,
+}) {
+  const domains = ["naver.com", "google.com", "kakao.com"];
+
+  return (
+    <div>
+      <label htmlFor="id" style={{ display: "inline-blockm", width: "80px" }}>
+        아이디
+      </label>
+      <input ref={idRef} type="text" value={id} onChange={onChangeEmail} />
+      {domain === "" ? null : <span>@</span>}
+      <select value={domain} onChange={onChangeDomain}>
+        {domains.map((d) => {
+          return (
+            <option key={d} value={d}>
+              {d}
+            </option>
+          );
+        })}
+        <option value="">직접입력</option>
+      </select>
+      {errors.idError && <div style={{ color: "red" }}>{errors.idError}</div>}
+    </div>
+  );
+}
