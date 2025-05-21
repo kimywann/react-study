268ffbae406b37aa8ac32971f5d4c2bd1aa333dc
kimywann <Kimywan10@gmail.com>
2025-05-21 22:50:59 +0900

    fix: Link 태그를 a 태그로 변경 - React Router의 Link 태그 대신 기본 a 태그 사용 - 회원가입/로그인 페이지 연결을 위한 href 속성 추가

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Link } from "react-router";
+
 function App() {
   return (
     <div>
-      <Link to="/signup">회원가입</Link>
-      <Link to="/login">로그인</Link> {/* a href 태그 대신 사용 */}
+      <a href="/signup">회원가입</a>
+      <a href="/login">로그인</a>
     </div>
   );
 }
