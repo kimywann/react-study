3c0cb5030f355e4aa5092b56cb6c2833469f3775
kimywann <kimywan10@gmail.com>
2025-05-21 22:03:28 +0900

    feat: 라우터 기본 설정 구현
    - BrowserRouter로 앱 래핑
    - Routes와 Route 컴포넌트를 사용한 라우팅 구조 설정
    - 메인, 로그인, 회원가입 페이지 라우트 설정

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Login from "./Login.jsx";
 import Signup from "./Signup.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById("root")).render(<Signup />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
