f1068da2862ed7c705e210eaf688d16a41eb5231
kimywann <Kimywan10@gmail.com>
2025-05-21 22:03:46 +0900

    feat: 메인 페이지 네비게이션 구현
    - Link 컴포넌트를 사용한 페이지 이동 구현
    - 회원가입과 로그인 페이지로 이동하는 링크 추가

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,60 +1,11 @@
-import { useState, useRef } from "react";
 import "./App.css";
-import useEmailInput from "./hooks/useEmailInput";
-import useInput from "./hooks/useInput";
-
-import EmailInput from "./components/EmailInput";
-import Input from "./components/Input";
-
+import { Link } from "react-router";
 function App() {
-  const { id, idRef, onChangeEmail, onChangeDomain, domain } = useEmailInput();
-  const [password, passwordRef, onChangePassword] = useInput("");
-  const countRef = useRef(0);
-  const [errors, setErrors] = useState({});
-
-  const fullDomain = `${id}@${domain}`;
-
-  const onLogin = () => {
-    countRef.current += 1;
-
-    if (!id?.trim()) {
-      setErrors({ idError: "아이디를 입력해주세요." });
-      idRef.current?.focus();
-      return;
-    }
-    if (!password?.trim()) {
-      setErrors({ passwordError: "비밀번호를 입력해주세요." });
-      passwordRef.current?.focus();
-      return;
-    }
-    setErrors({});
-    console.log(fullDomain, password);
-  };
-
   return (
-    <>
-      <div style={{ textAlign: "left" }} className="login-form">
-        <EmailInput
-          text="아이디"
-          id={id}
-          idRef={idRef}
-          domain={domain}
-          onChangeEmail={onChangeEmail}
-          onChangeDomain={onChangeDomain}
-          errors={errors}
-        />
-        <Input
-          text="비밀번호"
-          id="password"
-          ref={passwordRef}
-          type="password"
-          value={password}
-          onChange={onChangePassword}
-          errors={errors.passwordError}
-        />
-        <button onClick={onLogin}>로그인</button>
-      </div>
-    </>
+    <div>
+      <Link to="/signup">회원가입</Link>
+      <Link to="/login">로그인</Link> {/* a href 태그 대신 사용 */}
+    </div>
   );
 }
 
