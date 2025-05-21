7e27d1ce7eb3508f6ddc1d0c6bdc059e1c81b8ea
kimywann <kimywan10@gmail.com>
2025-05-21 22:49:10 +0900

    feat: ZeroRouter를 통한 라우팅 시스템 초기화 - React 앱의 진입점 설정 - ZeroRouter 컴포넌트를 루트에 마운트

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -1,17 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import Login from "./Login.jsx";
-import Signup from "./Signup.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
+import ZeroRouter from "./components/ZeroRouter";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<App />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")).render(<ZeroRouter />);
